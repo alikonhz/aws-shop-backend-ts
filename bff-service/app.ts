613fc79026c75fd027f664fdf66af6a6c624072f
alikonhz <123733442+alikonhz@users.noreply.github.com>
2024-01-03 21:56:58 +0100

    feat: task-10: add caching

--- bff-service/app.ts
+++ bff-service/app.ts
@@ -1,23 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import apicache from 'apicache';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cache = apicache.middleware;
 
 app.use('/cart', createProxyMiddleware({
     target: process.env.SERVICE_CART_URL,
     changeOrigin: true,
-    pathRewrite: {'^/cart' : ''}
+    pathRewrite: {'^/cart' : ''},
 }));
 
-app.use('/products', createProxyMiddleware({
+app.use('/products', [
+  express.Router().get('/', cache('2 minutes'), function(req,res,next) {
+    console.log('products (not cached): ', req.originalUrl);
+    next();
+  }),
+  createProxyMiddleware({
     target: process.env.SERVICE_PRODUCTS_URL,
     changeOrigin: true,
     pathRewrite: {'^/products' : ''}
-}));
+})
+]);
 
 app.use('/import', createProxyMiddleware({
     target: process.env.SERVICE_IMPORT_URL,
