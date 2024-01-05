4fed7a341102694c6d91168fc402b5f316233b0f
alikonhz <123733442+alikonhz@users.noreply.github.com>
2024-01-05 22:13:54 +0100

    feat: task-10

--- cart-service/bin/cart-service.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CartServiceStack } from '../lib/cart-service-stack';
-
-const app = new cdk.App();
-new CartServiceStack(app, 'CartServiceStack-DB', {});
